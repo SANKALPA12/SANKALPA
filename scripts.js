d6f4bdd7cfb09ad528336be547e584720270c8d9
SANKALPA12 <68837708+SANKALPA12@users.noreply.github.com>
2020-09-14 02:56:51 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,51 +1,59 @@
-let nCount = selector => {
-  $(selector).each(function () {
-    $(this)
-      .animate({
-        Counter: $(this).text()
-      }, {
-        // A string or number determining how long the animation will run.
-        duration: 4000,
-        // A string indicating which easing function to use for the transition.
-        easing: "swing",
-        /**
-         * A function to be called for each animated property of each animated element. 
-         * This function provides an opportunity to
-         *  modify the Tween object to change the value of the property before it is set.
-         */
-        step: function (value) {
-          $(this).text(Math.ceil(value));
-        }
-      });
-  });
-};
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
-let a = 0;
-$(window).scroll(function () {
-  // The .offset() method allows us to retrieve the current position of an element  relative to the document
-  let oTop = $(".numbers").offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() >= oTop) {
-    a++;
-    nCount(".rect > h1");
-  }
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
 });
 
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
 
 
-/**
- *
- *  sticky navigation
- *
- */
-
-let navbar = $(".navbar");
-
-$(window).scroll(function () {
-  // get the complete hight of window
-  let oTop = $(".section-2").offset().top - window.innerHeight;
-  if ($(window).scrollTop() > oTop) {
-    navbar.addClass("sticky");
-  } else {
-    navbar.removeClass("sticky");
-  }
-});
